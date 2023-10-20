6d069114d965e864a214ef01fe3e569a9232dd27
yolkleague <148539418+yolkleague@users.noreply.github.com>
2023-10-20 13:06:10 +0200

    Update Yolk League Modification.js

--- Yolk League Modification.js
+++ Yolk League Modification.js
@@ -10,11 +10,28 @@
 // ==/UserScript==
 
 (function() {
-	
+	const selectedItemPath = document.querySelector("#main-menu > menu > ul > li.home-menu-item > button > svg > path:nth-child(1)"); selectedItemPath.style.fill = "#00000000";
+	const selectedItemPath = document.querySelector("#main-menu > menu > ul > li.profile-menu-item > button > svg > path:nth-child(1)"); selectedItemPath.style.fill = "#00000000";
+	const selectedItemPath = document.querySelector("#main-menu > menu > ul > li.inventory-menu-item > button > svg > path:nth-child(1)"); selectedItemPath.style.fill = "#00000000";
+	const selectedItemPath = document.querySelector("#main-menu > menu > ul > li.shop-menu-item > button > svg > path:nth-child(1)"); selectedItemPath.style.fill = "#00000000";
+
 	const addScript = () => {
-		document.head.innerHTML +=
-			`<link rel="stylesheet" href="https://raw.githubusercontent.com/yolkleague/TampermonkeyModification/main/colors.css">`
-	}
+		document.head.innerHTML +=`
+  
+  		<title>Yolk League Shell Shockers</title>
+		<style>
+  			:root {
+				color: #ffffff;
+				--ss-blue1: #5865f2;
+				--ss-blue3: #ffffff;
+				--ss-blue5: #ffffff;
+				--ss-blue6: #5865f2;
+				--ss-yolk: #000000;
+				--ss-lightoverlay: radial-gradient(#ffffff, #5865f2);
+			}
+  		</style>
+    
+   	`}
 
 	document.body ? addScript() : document.addEventListener("DOMContentLoaded", e => addScript());
 	
