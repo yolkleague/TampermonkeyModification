865b19742c0cc2da1924c4a094166016afb41861
yolkleague <148539418+yolkleague@users.noreply.github.com>
2023-10-20 13:16:54 +0200

    Update Yolk League Modification.js

--- Yolk League Modification.js
+++ Yolk League Modification.js
@@ -4,20 +4,15 @@
 // @version      1
 // @description  Changes the design, adds a skybox and features.
 // @author       Yolk League
-// @match        *://shellshock.io/
-// @icon         https://raw.githubusercontent.com/yolkleague/TampermonkeyModification/main/icon.svg
+// @match        *://shellshock.io/*
+// @icon         https://raw.githubusercontent.com/yolkleague/TampermonkeyModification/main/Icon_Rounded.svg
 // @grant        none
 // ==/UserScript==
 
-(function() {
-	const homemenu = document.querySelector("#main-menu > menu > ul > li.home-menu-item > button > svg > path:nth-child(1)"); homemenu.style.fill = "#00000000";
-	const profilemenu = document.querySelector("#main-menu > menu > ul > li.profile-menu-item > button > svg > path:nth-child(1)"); profilemenu.style.fill = "#00000000";
-	const inventorymenu = document.querySelector("#main-menu > menu > ul > li.inventory-menu-item > button > svg > path:nth-child(1)"); inventorymenu.style.fill = "#00000000";
-	const shopmenu = document.querySelector("#main-menu > menu > ul > li.shop-menu-item > button > svg > path:nth-child(1)"); shopmenu.style.fill = "#00000000";
+(function() {const addScript = () => {
 
-	const addScript = () => {
 		document.head.innerHTML +=`
-  
+
   		<title>Yolk League Shell Shockers</title>
 		<style>
   			:root {
@@ -30,9 +25,9 @@
 				--ss-lightoverlay: radial-gradient(#ffffff, #5865f2);
 			}
   		</style>
-    
+
    	`}
 
 	document.body ? addScript() : document.addEventListener("DOMContentLoaded", e => addScript());
-	
+
 })();
